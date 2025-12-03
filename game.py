b43db98d00e6fa06c439dd4c7febe74d44df9ceb
MUMRIKK <noescribaaqui@gmail.com>
2025-12-03 17:26:48 +0300

    First_commit

---
+++ game.py
@@ -0,0 +1,42 @@
+import random , time
+
+
+
+comp_turn = ('k','n','b')
+
+def knb_choice(t):
+    if t=='k':
+        print('камень')
+    elif t=='n':
+        print('ножницы')
+    elif t=='b':
+        print('бумага')
+
+running = True
+
+def main():
+    global running
+
+    while running:
+        turn = input('Сделайте свой ход : k = камень, n = ножницы,  b =  бумага, e = выход    :')
+
+        time.sleep (0.5)
+
+
+
+        #else:
+        knb_choice(turn)
+        c_turn = random.choice(comp_turn)
+        knb_choice(c_turn)
+           if (turn == 'k' and c_turn == 'n') or (turn == 'n' and c_turn == 'b') or (turn == 'b' and c_turn == 'k'):
+
+
+
+           if (turn == 'n' and c_turn == 'k') or (turn == 'b' and c_turn == 'n') or (turn == 'k' and c_turn == 'b'):
+
+
+
+
+
+if __name__ == '__main__':
+    main()
