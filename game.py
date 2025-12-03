2f061230c29bd0f7319a1dd0ac90b01481983de7
MUMRIKK <noescribaaqui@gmail.com>
2025-12-03 18:59:31 +0300

    Add game Logic

--- game.py
+++ game.py
@@ -1,18 +1,19 @@
-import random , time
+import random, time
 
+comp_turn = ('k', 'n', 'b')
 
-
-comp_turn = ('k','n','b')
 
 def knb_choice(t):
-    if t=='k':
+    if t == 'k':
         print('камень')
-    elif t=='n':
+    elif t == 'n':
         print('ножницы')
-    elif t=='b':
+    elif t == 'b':
         print('бумага')
 
+
 running = True
+
 
 def main():
     global running
@@ -20,22 +21,26 @@ def main():
     while running:
         turn = input('Сделайте свой ход : k = камень, n = ножницы,  b =  бумага, e = выход    :')
 
-        time.sleep (0.5)
+        time.sleep(0.5)
 
+        if turn == 'e':
+            print(' Выход ')
+            time.sleep(5)
+            running = False
+        elif turn not in ('e', 'k', 'n', 'b'):
+            print(' Ошибка ввода, начните пожалуйста снова')
+            continue
 
+        else:
+            knb_choice(turn)
+            c_turn = random.choice(comp_turn)
+            knb_choice(c_turn)
+            if (turn == 'k' and c_turn == 'n') or (turn == 'n' and c_turn == 'b') or (turn == 'b' and c_turn == 'k'):
+                print('Вы выиграли!')
 
-        #else:
-        knb_choice(turn)
-        c_turn = random.choice(comp_turn)
-        knb_choice(c_turn)
-           if (turn == 'k' and c_turn == 'n') or (turn == 'n' and c_turn == 'b') or (turn == 'b' and c_turn == 'k'):
-
-
-
-           if (turn == 'n' and c_turn == 'k') or (turn == 'b' and c_turn == 'n') or (turn == 'k' and c_turn == 'b'):
-
-
-
+            # count [0] == ++1
+            if (turn == 'n' and c_turn == 'k') or (turn == 'b' and c_turn == 'n') or (turn == 'k' and c_turn == 'b'):
+                print('Я выиграл!')
 
 
 if __name__ == '__main__':
